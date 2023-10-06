9c78dd3f48d89e3a662e3cd80f10b142c67c63fa
KomarovaDiana <144255001+KomarovaDi@users.noreply.github.com>
2023-10-06 19:16:02 +0300

    add files into project

---
+++ script.js
@@ -0,0 +1,23 @@
+// массивы
+// var arr = [4, true, -199, 3.4, 0, "stroka"];
+// arr [4] = "ok" ;
+// console.log(arr[arr, length]); 
+
+// var matrix = [
+//     [3, 5, 7], [2, 9, 10], [ -199, 3.4,]
+// ];
+
+// matrix [1] [0] = 90; 
+// console.log(matrix);
+
+
+// цикл for 
+// for (var i = 100; i > 5;  i/= 2) {
+//     console.log(i);
+// }
+
+var j = 1000;
+while (j >= 100) {
+    console.log(j);
+    j -= 100;
+}
